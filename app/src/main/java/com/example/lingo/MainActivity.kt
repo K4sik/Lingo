73ddc419f13cce8f0b2faab2cd42118c8f5d36e7
K4sik <kasiks597@gmail.com>
2021-04-14 16:40:27 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lingo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.lingo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
