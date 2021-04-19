4dd3d572addfe1eff1481316592587b3ffcf3962
K4sik <kasiks597@gmail.com>
2021-04-19 22:59:05 +0300

    Added method onClick

--- app/src/main/java/com/example/lingo/adapters/SettingsAdapter.java
+++ app/src/main/java/com/example/lingo/adapters/SettingsAdapter.java
@@ -1,5 +1,7 @@
 package com.example.lingo.adapters;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +12,9 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.lingo.R;
+import com.example.lingo.activities.PodcastActivity;
+import com.example.lingo.activities.PracticeActivity;
+import com.example.lingo.activities.TheoryActivity;
 import com.example.lingo.models.Settings;
 
 import java.util.ArrayList;
@@ -43,17 +48,48 @@ public class SettingsAdapter extends RecyclerView.Adapter<SettingsAdapter.ViewHo
         return dataholder.size();
     }
 
-    class ViewHolder extends RecyclerView.ViewHolder
-    {
+    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private final Context context;
         ImageView img;
         TextView header;
         ImageView imgStat;
         public ViewHolder(@NonNull View itemView)
         {
             super(itemView);
+            context = itemView.getContext();
             img = itemView.findViewById(R.id.iv_image);
             header = itemView.findViewById(R.id.tv_title);
             imgStat = itemView.findViewById(R.id.iv_arrow);
+
+//            itemView.setClickable(true);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+
+            Intent mailIntent;
+
+            switch (getAdapterPosition()){
+
+                case 0:
+                    mailIntent = new Intent(Intent.ACTION_SEND);
+                    mailIntent.setType("text/plain");
+                    mailIntent.putExtra(Intent.EXTRA_EMAIL, new String[] { "roman.kasarab.pz.2018@lpnu.ua" });
+                    mailIntent.putExtra(Intent.EXTRA_SUBJECT, "Lingo");
+                    mailIntent.putExtra(Intent.EXTRA_TEXT, "Best app for learn English :)");
+
+                    context.startActivity(Intent.createChooser(mailIntent, "Send Email"));
+                    break;
+
+                case 1:
+
+                    break;
+
+                default:
+
+                    break;
+            }
         }
     }
 }
